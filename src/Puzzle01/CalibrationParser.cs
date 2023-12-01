9fd42aa0f581ddebdfd8e538b85d8265d2979e9a
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-01 09:22:03 +0000

    Partial implementation for Puzzle 1 Part 2

--- src/Puzzle01/CalibrationParser.cs
+++ src/Puzzle01/CalibrationParser.cs
@@ -4,6 +4,13 @@
 
 namespace Puzzle1;
 
+using System.Text.RegularExpressions;
+public enum ParsingMode
+{
+    Numeric,
+    Semantic,
+}
+
 public class CalibrationParser
 {
     private string CalibrationFilePath { get; set; }
@@ -13,13 +20,16 @@ public class CalibrationParser
         CalibrationFilePath = calibrationFilePath;
     }
 
-    public void ParseCalibrationFile()
+    public int ParseCalibrationFile(ParsingMode mode)
     {
         var calibrationValue = 0;
         var lineDigits = new List<int>();
+
         foreach(var line in File.ReadLines(CalibrationFilePath))
         {
-            foreach(char character in line)
+            var parsedLine = (mode == ParsingMode.Semantic) ? ParseLineSemanticly(line) : line;
+
+            foreach(char character in parsedLine)
             {
                 if(Char.IsDigit(character))
                     lineDigits.Add( (int)Char.GetNumericValue(character) );
@@ -28,7 +38,39 @@ public class CalibrationParser
                 calibrationValue += lineDigits.Last() + 10 * lineDigits.First();
             lineDigits.Clear();
         }
-        Console.WriteLine($@"Calibration file result is: {calibrationValue}");
+
+        return calibrationValue;
+    }
+
+    public static string ParseLineSemanticly(string line)
+    {
+        var regexSearchString = "one|two|three|four|five|six|seven|eight|nine";
+        var regexSearch = new Regex(regexSearchString);
+        Match match;
+        do
+        {
+            match = Regex.Match(line, regexSearchString);
+            line = regexSearch.Replace(line, ReplaceWordWithNumber, 1);
+        } while (match.Success);
+        return line;
+    }
+
+    public static string ReplaceWordWithNumber(Match match)
+    {
+        var number = match.Value switch
+        {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => throw new Exception("Error with string matching")
+        };
+        return number;
     }
 }
 
@@ -37,6 +79,11 @@ class Program
     static void Main(string[] args)
     {
         var calibrationFile = new CalibrationParser(args[0]);
-        calibrationFile.ParseCalibrationFile();
+
+        var calibrationValueNumeric = calibrationFile.ParseCalibrationFile(ParsingMode.Numeric);
+        var calibrationValueSemantic = calibrationFile.ParseCalibrationFile(ParsingMode.Semantic);
+
+        Console.WriteLine($@"Calibration file result  with numeric parsing is: {calibrationValueNumeric}");
+        Console.WriteLine($@"Calibration file result  with semantic parsing is: {calibrationValueSemantic}");
     }
 }
