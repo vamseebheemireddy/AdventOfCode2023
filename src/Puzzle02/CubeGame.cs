98685c159532a5e909de195d24815cbaeb11442a
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-02 17:14:54 +0000

    Created skeleton template code for Puzzle 2

---
+++ src/Puzzle02/CubeGame.cs
@@ -0,0 +1,25 @@
+﻿// CalibrationParser
+
+// Parse calibration file to return sum of codes
+
+namespace Puzzle2;
+
+using System.Text.RegularExpressions;
+public class CubeGame
+{
+    private string GameFilePath { get; set; }
+
+    public CubeGame(string gameFilePath)
+    {
+        GameFilePath = gameFilePath;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gameFile = new CubeGame(args[0]);
+
+    }
+}
