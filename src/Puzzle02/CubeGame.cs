6c356c6f25c9ca0c91f05bdf33682b09b3aaefb9
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-02 18:17:45 +0000

    Partial implementation for Puzzle 2 Part 1

--- src/Puzzle02/CubeGame.cs
+++ src/Puzzle02/CubeGame.cs
@@ -5,13 +5,53 @@
 namespace Puzzle2;
 
 using System.Text.RegularExpressions;
+
+public readonly record struct Draw(uint Red, uint Green, uint Blue);
+
+public record struct Game(uint Id, List<Draw> Draws);
 public class CubeGame
 {
     private string GameFilePath { get; set; }
+    private List<Game> Games { get; set; };
 
     public CubeGame(string gameFilePath)
     {
         GameFilePath = gameFilePath;
+        Games = new List<Game>();
+    }
+
+    public void ParseGameFile()
+    {
+        foreach(var line in File.ReadLines(GameFilePath))
+        {
+            var gameText = line.Split(":");
+            if (!gameText[0].Contains("Game"))
+            {
+                throw new Exception($"Input file syntax error. Line: {line}");
+            } 
+            else
+            {
+                var id = Convert.ToUInt32(gameText[0].Replace("Game ", ""));
+            }
+
+            var draws = ParseDraws(gameText[1]);
+        }
+    }
+
+    private List<Draw> ParseDraws(string gameText)
+    {
+        var draws = new List<Draw>();
+        var drawTexts = gameText.Split(";");
+        foreach (var drawText in drawTexts)
+        {
+            var colourTexts = drawText.Split(",");
+            foreach (var colourText in colourTexts)
+            {
+                switch colourText
+            }
+        }
+        return draws;
+        
     }
 }
 
