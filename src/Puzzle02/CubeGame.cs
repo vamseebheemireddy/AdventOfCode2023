74712bec195300fee5ac9a6508bf5cf1a77d8706
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-02 20:22:56 +0000

    Complete solution to Puzzle 02 Part 1

--- src/Puzzle02/CubeGame.cs
+++ src/Puzzle02/CubeGame.cs
@@ -6,13 +6,15 @@ namespace Puzzle2;
 
 using System.Text.RegularExpressions;
 
-public readonly record struct Draw(uint Red, uint Green, uint Blue);
+#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
 
-public record struct Game(uint Id, List<Draw> Draws);
+public record struct Draw(int Red, int Green, int Blue);
+
+public record struct Game(int Id, List<Draw> Draws);
 public class CubeGame
 {
     private string GameFilePath { get; set; }
-    private List<Game> Games { get; set; };
+    private List<Game> Games { get; set; }
 
     public CubeGame(string gameFilePath)
     {
@@ -24,6 +26,7 @@ public class CubeGame
     {
         foreach(var line in File.ReadLines(GameFilePath))
         {
+            int id;
             var gameText = line.Split(":");
             if (!gameText[0].Contains("Game"))
             {
@@ -31,27 +34,67 @@ public class CubeGame
             } 
             else
             {
-                var id = Convert.ToUInt32(gameText[0].Replace("Game ", ""));
+                id = Convert.ToInt32(gameText[0].Replace("Game ", ""));
             }
 
             var draws = ParseDraws(gameText[1]);
+            Games.Add(new Game(id, draws));
         }
     }
 
-    private List<Draw> ParseDraws(string gameText)
+    private static List<Draw> ParseDraws(string gameText)
     {
         var draws = new List<Draw>();
         var drawTexts = gameText.Split(";");
+
         foreach (var drawText in drawTexts)
         {
+            var draw = new Draw(Red: 0, Green: 0, Blue: 0);
             var colourTexts = drawText.Split(",");
+
             foreach (var colourText in colourTexts)
             {
-                switch colourText
+                var colour = Regex.Match(colourText, @"red|green|blue").Value;
+                switch (colour)
+                {
+                    case "red": 
+                        draw.Red = Convert.ToInt32(Regex.Match(colourText, @"\d+").Value);
+                        break;
+                    case "green": 
+                        draw.Green = Convert.ToInt32(Regex.Match(colourText, @"\d+").Value);
+                        break;
+                    case "blue": 
+                        draw.Blue = Convert.ToInt32(Regex.Match(colourText, @"\d+").Value);
+                        break;
+                    default:
+                        throw new Exception($@"Failed to find number of colour {colour} in entry {colourText}.");
+                }
             }
+            draws.Add(draw);
         }
+
         return draws;
-        
+    }
+
+    public List<int> FindValidGames(Draw totalCubes)
+    {
+        var validGames = new List<int>();
+        foreach (var game in Games)
+        {
+            if (AllDrawsValid(game.Draws, totalCubes))
+                validGames.Add(game.Id);
+        }
+        return validGames;
+    }
+
+    private static bool AllDrawsValid(List<Draw> draws, Draw totalCubes)
+    {
+        foreach (var draw in draws)
+        {
+            if (draw.Red > totalCubes.Red || draw.Green > totalCubes.Green || draw.Blue > totalCubes.Blue)
+                return false;
+        }
+        return true;
     }
 }
 
@@ -59,7 +102,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        var gameFile = new CubeGame(args[0]);
-
+        var totalCubes = new Draw(Red: Convert.ToInt32(args[1]), Green: Convert.ToInt32(args[2]), Blue: Convert.ToInt32(args[3]));
+        var cubeGame = new CubeGame(args[0]);
+        cubeGame.ParseGameFile();
+        var validGames = cubeGame.FindValidGames(totalCubes);
+        Console.WriteLine($@"Sum of valid game IDs is: {validGames.Sum()}");
     }
 }
+
+#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
