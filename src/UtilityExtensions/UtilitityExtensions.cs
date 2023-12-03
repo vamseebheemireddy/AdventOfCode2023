48cb0f23cfa69f81d1a45852f3f864658a43b9a3
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-03 13:20:53 +0000

    Added extension for index use in foreach loops

---
+++ src/UtilityExtensions/UtilitityExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace UtilityExtensions;
+
+using System.Collections.Generic;
+
+public static class IEnumerableExtensions {
+    public static IEnumerable<(T item, int index)> Enumerated<T>(this IEnumerable<T> self)       
+       => self.Select((item, index) => (item, index));
+}
