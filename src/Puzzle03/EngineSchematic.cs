ce87b74e9134ac5dc1f122b5d545fdcd056372f4
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-03 15:54:18 +0000

    Complete solution to Pauzzle 03 Part 1

--- src/Puzzle03/EngineSchematic.cs
+++ src/Puzzle03/EngineSchematic.cs
@@ -2,30 +2,72 @@
 
 // Parse engine schematic file and find sum of valid parts
 
-namespace Puzzle3;
+namespace Puzzle03;
 
 using UtilityExtensions;
 using System.Text.RegularExpressions;
 
-public record struct Part(int Line, int Position, int PartNumber);
+#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
+
+public record struct Part(int Line, int StartPosition, int EndPosition, int PartNumber);
 
 public class EngineSchematic
 {
-    private string[] schematic;
+    private readonly string[] schematic;
 
-    private List<Part> parts;
+    private readonly List<Part> parts;
 
     public EngineSchematic(string schematicFilePath)
     {
-        schematic = (string[])File.ReadLines(schematicFilePath);
+        schematic = File.ReadAllLines(schematicFilePath);
         parts = new List<Part>();
     }
 
     public int FindValidParts()
     {
+        foreach(var (line, index) in schematic.Enumerated())
+        {
+            var partMatch = Regex.Match(line, @"\d+");
 
+            while (partMatch.Success)
+            {
+                var newPart = new Part(index, partMatch.Index, partMatch.Index + partMatch.Length - 1, Convert.ToInt32(partMatch.Value));
+                if (IsValidPart(newPart))
+                    parts.Add(newPart);
+                partMatch = partMatch.NextMatch();
+            }
+        }
+
+        var partsTotal = parts.Select(x => x.PartNumber).Sum();
+        return partsTotal;
     }
 
+    private bool IsValidPart(Part part)
+    {
+        var line = schematic[part.Line];
+
+        if (part.StartPosition > 0)
+            if (line.Substring(part.StartPosition - 1, 1) != ".")
+                return true;
+
+        if (part.EndPosition < line.Length - 1)
+            if (line.Substring(part.EndPosition + 1, 1) != ".")
+                return true;
+
+        var leftEdge = part.StartPosition - 1 < 0 ? 0 : part.StartPosition - 1;
+        var rightEdge = part.EndPosition + 1 > line.Length - 1 ? line.Length - 1 : part.EndPosition + 1;
+        var noSymbol = new string ('.', rightEdge - leftEdge + 1);
+
+        if (part.Line > 0)
+            if (!Equals(schematic[part.Line - 1].Substring(leftEdge, rightEdge - leftEdge + 1), noSymbol))
+                return true;
+
+        if (part.Line < schematic.Length - 1)
+            if (!Equals(schematic[part.Line + 1].Substring(leftEdge, rightEdge - leftEdge + 1), noSymbol))
+                return true;
+
+        return false;
+    }
 }
 
 class Program
@@ -38,3 +80,5 @@ class Program
         Console.WriteLine($@"Sum of valid part numbers is: {partTotal}");
     }
 }
+
+#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
