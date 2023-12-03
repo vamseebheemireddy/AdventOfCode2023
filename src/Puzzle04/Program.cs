c467e68009bb0f32a3e7c0499f50e099d89e24d6
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-03 18:18:07 +0000

    Setup configuration for Puzzle 04

--- src/Puzzle04/Program.cs
+++ src/Puzzle04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Name
+
+// Summary
+
+namespace Puzzle04;
+
+using UtilityExtensions;
+using System.Text.RegularExpressions;
+
+#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
+
+public class SolutionClass
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
