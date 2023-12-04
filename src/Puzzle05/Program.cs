aa17f6e4b9f179a23c0f2ee2341acfbc44a3e739
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-04 17:51:54 +0000

    Setup configuration for Puzzle 05

--- src/Puzzle05/Program.cs
+++ src/Puzzle05/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Name
+
+// Summary
+
+namespace Puzzle05;
+
+using UtilityExtensions;
+using System.Text.RegularExpressions;
+
+#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
+
+public class Class
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
