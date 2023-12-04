800993980cb66dec89dd4f154ebd3a11e3225f3a
Vamsee Bheemireddy <40067528+vamseebheemireddy@users.noreply.github.com>
2023-12-04 17:47:08 +0000

    Complete solution to Puzzle 04 Part 2

--- src/Puzzle04/Scratchcard.cs
+++ src/Puzzle04/Scratchcard.cs
@@ -11,22 +11,22 @@ using System.Text.RegularExpressions;
 
 public class Scratchcard
 {
-    private List<int> PlayerNumbers;
-    private List<int> WinningNumbers;
-    private int Points;
+    public readonly int Id;
+    public int Copies;
+    public readonly int Matches;
 
-    public Scratchcard(List<int> playerNumbers, List<int> winningNumbers, int points)
+    public Scratchcard(int id, int matches)
     {
-        PlayerNumbers = playerNumbers;
-        WinningNumbers = winningNumbers;
-        Points = points;
+        Id = id;
+        Copies = 1;
+        Matches = matches;
     }
 
     public static List<Scratchcard> ParseScratchcardFile(string scratchcardsFilePath)
     {
         var cards = new List<Scratchcard>();
 
-        foreach (var line in File.ReadLines(scratchcardsFilePath))
+        foreach (var (line, id) in File.ReadAllLines(scratchcardsFilePath).Enumerated())
         {
             var allNumbersText = line.Split(":")[1];
             var winningNumbersText = allNumbersText.Split("|")[0];
@@ -48,26 +48,15 @@ public class Scratchcard
                 playerNumbersMatch = playerNumbersMatch.NextMatch();
             }
 
-            cards.Add(new Scratchcard(playerNumbers, winningNumbers, 0));
+            var matches = 0;
+            foreach (var winningNumber in  winningNumbers)
+                if (playerNumbers.Contains(winningNumber))
+                    matches += 1;
+
+            cards.Add(new Scratchcard(id, matches));
         }
 
         return cards;
-    }
-
-    public int GetPoints()
-    {
-        Points = 0;
-        foreach (var winningNumber in WinningNumbers)
-        {
-            if (PlayerNumbers.Contains(winningNumber))
-            {
-                if (Points == 0)
-                    Points = 1;
-                else
-                    Points *= 2; 
-            }
-        }
-        return Points;
     }
 }
 
@@ -76,8 +65,19 @@ class Program
     static void Main(string[] args)
     {
         var cards = Scratchcard.ParseScratchcardFile(args[0]);
-        var totalPoints = cards.Select(x => x.GetPoints()).Sum();
+        var totalPoints = cards.Select(x => x.Matches == 0 ? 0 : 1 << x.Matches - 1).Sum();
         Console.WriteLine($@"Sum of scratchcard points is: {totalPoints}");
+
+        foreach(var (card, id) in cards.Enumerated())
+        {
+            var startCopy = id < cards.Count - 1 ? id + 1 : -1;
+            var endCopy = id + card.Matches < cards.Count ? id + card.Matches : cards.Count - 1;
+            if (startCopy != -1)
+                foreach (var copyId in Enumerable.Range(startCopy, endCopy - startCopy + 1))
+                    cards[copyId].Copies += card.Copies;
+        }
+        var totalCards = cards.Select(x => x.Copies).Sum();
+        Console.WriteLine($@"Total scratchcard copies is: {totalCards}");
     }
 }
 
